fd3f036b9678f080240315c71e59ab7889ebe5ee
Dhiraj Mehta <mehtadhiraj21@gmail.com>
2018-07-05 21:23:31 +0530

    Grunt and gulp complete

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,16 @@
 'use strict';
 
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync');
+var gulp 		= require('gulp'),
+	sass 		= require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	del 		= require('del'),
+	imagemin	= require('gulp-imagemin'),
+	usemin		= require('gulp-usemin'),
+	uglify		= require('gulp-uglify'),
+	htmlmin		= require('gulp-htmlmin'),
+	rev 		= require('gulp-rev'),
+	cleanCss 	= require('gulp-clean-css'),
+	flatmap		= require('gulp-flatmap');
 
 gulp.task('sass', function(){
 	return gulp.src('./css/*.scss')
@@ -28,7 +36,46 @@ gulp.task('browser-sync', function() {
 		}
 	});
 });
-
+ //Default task
 gulp.task('default', ['browser-sync'], function(){
 	gulp.start('sass: watch');
-})
+});
+
+gulp.task('clean', function(){
+	return del(['dist']);
+});
+
+gulp.task('copyfonts', function(){
+	gulp.src('node_modules/font-awesome/fonts/**/*.{ttf,wof,eof,svg}*')
+	.pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', function(){
+	return gulp.src('./img/*.{png, jpg, gif}')
+	.pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
+	.pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('usemin', function(){
+	return gulp.src('./*.html')
+	.pipe(flatmap(function(stream, file){
+		return stream
+		.pipe(usemin({
+			css: [rev()],
+			html: [function(){
+				return htmlmin({
+					collapseWhitespace: true
+				});
+			}],
+			js: [uglify(), rev()],
+			inlinejs: [uglify()],
+			inlinecss: [cleanCss(), 'concat']
+		}))
+	}))
+	.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean'], function(){
+	gulp.start('copyfonts','imagemin','usemin');
+});
+
